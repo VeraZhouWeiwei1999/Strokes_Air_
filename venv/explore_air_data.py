5afa14dfdbcaec360c80cdbbc068a2f2b80552c3
KrisVolkova <83023006+KrisVolkova@users.noreply.github.com>
2023-10-05 11:19:11 +0300

    Update explore_air_data.py

--- venv/explore_air_data.py
+++ venv/explore_air_data.py
@@ -1,22 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from enum import Enum
+import utils
+import setting from Setting 
+
+class Colums(str, Enum):
+    DATE = 'Date'
 
 pd.options.display.max_columns = None
 
 # check the data, think about how the data from strokes and air datasets can be used
 # group the data by dates, locations
-
-def get_data(route):
-    df = pd.read_csv(route)
-    return df
-
-df_air = get_data("./data for project/Air_data/joined_data.csv")
+# Rename
+df_air = utils.get_data(setting.base_path + "joined_data.csv")
 # df_air = get_data("./data for project/Air_data/joined_dat.csv")
 
 # select 2013, 2014, 2016
 def select_dates(dates):
     df_dates = pd.DataFrame(
-        columns=['Date', 'AQI', 'Category', 'Defining Parameter', 'city_ascii', 'state_id', 'population'])
+        columns=[Colums.DATE, 'AQI', 'Category', 'Defining Parameter', 'city_ascii', 'state_id', 'population'])
      #   columns=['Date', 'AQI', 'Category', 'Defining Parameter', 'city_ascii', 'state_id', 'state_name', 'population'])
     for year in dates:
         start_date = f'{year}-01-01'
@@ -66,11 +68,10 @@ def explore_data_year(parameter):
     df = df.reset_index()
     return df
 air_mean_per_year_df = explore_data_year("mean")
-def save_data(df_name, directory, name):
-    df_name.to_csv(f"./data for project/{directory}/{name}.csv")
-    print(f"The data saved to {directory}")
-    return
-save_data(air_mean_per_year_df, "combined", "air_mean_per_year")
+
+
+utils.save_data(air_mean_per_year_df, "combined", "air_mean_per_year")
+
 def create_plot(df, city):
     df['date'] = df['Month'].map(str) + '-' + df['Year'].map(str)
     df['date'] = pd.to_datetime(df['date'], format='%m-%Y').dt.strftime('%m-%Y')
